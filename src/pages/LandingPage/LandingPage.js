b93ff1fcbe3c30e85d87d90a490126e4cd71058c
Mondo3771 <kabelomrn@gmail.com>
2024-04-14 18:31:15 +0200

    trust the boi nathan

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,178 +1,158 @@
-
-
 // export default LandingPage;
-import React, { useState } from 'react';
-import Index from '../../routes/Index';
+import React, { useState } from "react";
+import Index from "../../routes/Index";
 
-import { InputContainer, Wrapper, Card, LandingPageContainer } from './LandingPage.styles';
+import {
+  InputContainer,
+  Wrapper,
+  Card,
+  LandingPageContainer,
+} from "./LandingPage.styles";
 
 const LandingPage = () => {
   const [login, setLogin] = useState(false);
   const [signup, setSignup] = useState(false);
-  const[first,setFirst]=useState(false);
+  const [first, setFirst] = useState(false);
 
   const handleLogin = () => {
-    setLogin(prevLogin => !prevLogin);
+    setLogin((prevLogin) => !prevLogin);
     setSignup(false);
     setFirst(true);
   };
 
   const handleSignup = () => {
-    setSignup(prevSignup => !prevSignup);
+    setSignup((prevSignup) => !prevSignup);
     setLogin(false);
     setFirst(true);
-
   };
 
-  const [Department,setDepartment]=useState('');
-  const [EmpType,setEmpType]=useState('');
+  const [Department, setDepartment] = useState("");
+  const [EmpType, setEmpType] = useState("");
 
-const depChange=(event)=>{
-  setDepartment(event.target.value);
-}
+  const depChange = (event) => {
+    setDepartment(event.target.value);
+  };
 
-const typeChange=(event)=>{
-  setEmpType(event.target.value);
-}
+  const typeChange = (event) => {
+    setEmpType(event.target.value);
+  };
 
+  const [data, setData] = useState("");
+  const childToParent = (childdata) => {
+    setData(childdata);
+  };
 
+  const [Error, setError] = useState("Create Account");
+  const [logError, setlogError] = useState("Log in with your Google account");
+  const [DBsign, setDBsign] = useState({}); // What goes into the databse
+  const [DBlog, setDBlog] = useState({}); // querying database wiht email and name
 
-const [data, setData] = useState('');
-const childToParent = (childdata) => {
-  setData(childdata);
-}
+  const sign = () => {
+    if (!Department || !EmpType) {
+      return setError("Enter All Fields");
+    } else if (!data.email) {
+      return setError("Select Account");
+    } else if (!data.email_verified) {
+      return setError("Unverified a Account");
+    } else {
+      setError("Welcome " + data.name + "! Click Sign Up");
+      setDBsign({
+        Department: Department,
+        employeeType: EmpType,
+        email: data.email,
+        Name: data.given_name,
+        Surname: data.family_name,
+      });
+      // FETCH POST REQUEST TO DATABASE USE LOG
+      // PUT dbsign IN A BODY FRO YOUR REQUEST
+    }
+    console.log(DBsign);
+  };
+  const log = () => {
+    if (!data.email) {
+      return setlogError("Select Account");
+    } else if (!data.email_verified) {
+      return setlogError("Unverified a Account");
+    } else {
+      setlogError("Waiting for " + data.name + "! Click Login");
+      setDBlog({
+        email: data.email,
+        Name: data.given_name,
+      });
 
-const [Error,setError]=useState('Create Account');
-const [logError,setlogError]=useState('Log in with your Google account')
-const [DBsign,setDBsign]=useState({});// What goes into the databse
-const [DBlog,setDBlog]=useState({});// querying database wiht email and name
+    }
 
-
-const sign=()=>{
-  if(!Department || !EmpType){
-    return setError('Enter All Fields')
-  }
-   else if(!data.email){
-    return setError('Select Account')
-  }
-  else if(!data.email_verified){
-    return setError('Unverified a Account')
-
-  }
-
-  else{
-    setError('Welcome '+data.name +'! Click Sign Up');
-    setDBsign({
-      "Department":Department,
-      "employeeType":EmpType,
-      "email":data.email,
-      "Name":data.given_name,
-      "Surname":data.family_name
-
-
-  })
-  }
-
-  console.log(DBsign);
-
-
-}
-const log=()=>{
-   if(!data.email){
-    return setlogError('Select Account')
-  }
-  else if(!data.email_verified){
-    return setlogError('Unverified a Account')
-
-  }
-
-  else{
-    setlogError('Waiting for '+data.name +'! Click Login');
-    setDBlog({
-  
-      "email":data.email,
-      "Name":data.given_name
-
-
-  })
-  }
-
-  console.log(DBlog);
-  
-}
+    console.log(DBlog);
+  };
 
   return (
     <>
       <LandingPageContainer>
         <Wrapper>
           <Card>
-            <section className='top'>
-              <img src={require('./logo.png')} alt="Logo" />
+            <section className="top">
+              <img src={require("./logo.png")} alt="Logo" />
               <h1>SYNERGY</h1>
             </section>
-            <section className='bottom'>
-              
+            <section className="bottom">
               {login ? (
                 <>
-                <article className='secLog'>
-                  <h2>{logError}</h2>
-                 { first?
-                    <Index child={childToParent}/>:null
+                  <article className="secLog">
+                    <h2>{logError}</h2>
+                    {first ? <Index child={childToParent} /> : null}
+                    <button className="log" onClick={log}>
+                      Login
+                    </button>
+                  </article>
 
-                  }
-                    <button className='log' onClick={log}>Login</button>
-
-                  
-
-                </article>
-                
-                <button className='login' onClick={handleSignup}>Sign Up</button>
-
+                  <button className="login" onClick={handleSignup}>
+                    Sign Up
+                  </button>
                 </>
-               
-            ) : (
-              <>
-               
-                  <button className='login' onClick={handleLogin}>Login</button>
-                  
+              ) : (
+                <>
+                  <button className="login" onClick={handleLogin}>
+                    Login
+                  </button>
+                  {" "}
+                  <article className="secSign">
+                    <h2>{Error}</h2>
+                    <InputContainer>
+                      <img src={require("./profile.png")}></img>
+                      <input
+                        type="text"
+                        placeholder="Department"
+                        value={Department}
+                        onChange={depChange}
+                      />
+                    </InputContainer>
+                    <InputContainer>
+                      <img src={require("./profile.png")}></img>
+                      {/* <input type='text' placeholder='Employee Type' value={EmpType} onChange={typeChange} /> */}
+                      <select
+                        value={EmpType}
+                        onChange={typeChange}
+                        placeholder="Employee type"
+                      >
+                        <option value="" disabled hidden>
+                          Select Employee Type
+                        </option>
 
-=                <article className='secSign'>
-                      <h2>{Error}</h2>
-                        <InputContainer>
-
-                        <img src={require('./profile.png')}></img>
-                          <input type='text' placeholder='Department' value={Department} onChange={depChange} />
-
-                        </InputContainer>
-                        <InputContainer>
-                        <img src={require('./profile.png')}></img>
-                        {/* <input type='text' placeholder='Employee Type' value={EmpType} onChange={typeChange} /> */}
-                        <select value={EmpType} onChange={typeChange} placeholder='Employee type'>
-                        <option value="" disabled hidden>Select Employee Type</option>
-
-                          <option value="Staff">Staff</option>
-                          <option value="Manager">Manager</option>
-                          <option value="HR">HR</option>
+                        <option value="Staff">Staff</option>
+                        <option value="Manager">Manager</option>
+                        <option value="HR">HR</option>
                       </select>
+                    </InputContainer>
+                    <Index child={childToParent} />
 
-                        </InputContainer>
-                        <Index child={childToParent}/>
-
-                        <button className='sign' onClick={sign}>Sign Up</button>
-
-
-                </article>
-           
-           
-                
-              </>
-             
-            )}
+                    <button className="sign" onClick={sign}>
+                      Sign Up
+                    </button>
+                  </article>
+                </>
+              )}
             </section>
-            
-           
           </Card>
-         
         </Wrapper>
       </LandingPageContainer>
     </>
@@ -180,4 +160,3 @@ const log=()=>{
 };
 
 export default LandingPage;
-
